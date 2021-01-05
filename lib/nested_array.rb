151fbf97779dec96a75f1764f7d47fa542ae11ee
Andrew  Perlis <ajperlis07@gmail.com>
2021-01-05 23:59:23 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -49,7 +49,7 @@ def matrix_lookup(matrix, row, column)
 end
 
 def matrix_update(matrix, row, column, new_value)
- matrix_update = (matrix[row][column] = [new_value])
+ matrix_update = (matrix[row][column] = new_value)
  matrix_update
   
   # Given any matrix (array of arrays), a row index and a column index, 
